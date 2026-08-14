48dab062951d1dff8d2402a29025bc7f79c21e07
pksking <kprithiv1010@gmail.com>
2026-08-14 18:15:41 +0530

    Fix splash screen and update assets

--- app.config.js
+++ app.config.js
@@ -7,7 +7,7 @@ module.exports = {
     icon: './assets/icon.png',
     userInterfaceStyle: 'light',
     splash: {
-      image: './assets/splash.png',
+      image: './assets/icon.png',
       resizeMode: 'contain',
       backgroundColor: '#ffffff',
     },
